6edf45d19a3109a0244f0d76863117805373bb24
mansursultoni <sultoni.pyar@gmail.com>
2021-08-05 13:57:09 +0700

    Membuat RegisterAdmin

--- app/src/main/java/com/mansursultoni/desakarangtalunlor/LoginAdmin.java
+++ app/src/main/java/com/mansursultoni/desakarangtalunlor/LoginAdmin.java
@@ -2,13 +2,28 @@ package com.mansursultoni.desakarangtalunlor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginAdmin extends AppCompatActivity {
+
+    TextView tvDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
+
+        tvDaftar = findViewById(R.id.tvDaftar);
+        tvDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginAdmin.this, RegisterAdmin.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
