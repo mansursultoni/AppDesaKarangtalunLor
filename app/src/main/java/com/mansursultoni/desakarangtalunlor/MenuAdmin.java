865f52842495284bec47d4105dfee23462b8aa8e
mansursultoni <sultoni.pyar@gmail.com>
2021-08-05 20:31:07 +0700

    Menambah tombol logout MenuAdmin dan MenuUser

--- app/src/main/java/com/mansursultoni/desakarangtalunlor/MenuAdmin.java
+++ app/src/main/java/com/mansursultoni/desakarangtalunlor/MenuAdmin.java
@@ -2,13 +2,29 @@ package com.mansursultoni.desakarangtalunlor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuAdmin extends AppCompatActivity {
+
+    Button btLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+
+        btLogout = findViewById(R.id.btLogout);
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuAdmin.this, LoginAdmin.class));
+                preferences.clearData(MenuAdmin.this);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/mansursultoni/desakarangtalunlor/MenuUser.java
+++ app/src/main/java/com/mansursultoni/desakarangtalunlor/MenuUser.java
@@ -2,13 +2,30 @@ package com.mansursultoni.desakarangtalunlor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuUser extends AppCompatActivity {
+
+    Button btLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_user);
+
+        btLogout = findViewById(R.id.btLogout);
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuUser.this, LoginAdmin.class));
+                preferences.clearData(MenuUser.this);
+                finish();
+            }
+        });
+
     }
 }
